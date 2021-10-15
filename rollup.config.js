f73ea76a0878486969eaaec2d1adc680d431bf3c
Luyou <luyou@gaoding.com>
2021-10-15 10:06:09 +0800

    foo:test

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,52 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
-import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-const outputDir = "dist/";
+const outputDir = 'dist/'
 
 export default [
   {
-    input: "src/index.ts", // 打包入口
+    input: 'src/index.ts', // 打包入口
     output: [
       {
         file: outputDir + pkg.main,
-        format: "commonjs",
+        format: 'commonjs'
       },
       {
         file: outputDir + pkg.module,
-        format: "esm",
+        format: 'esm'
       },
       {
-        file: outputDir + "index.min.js",
-        format: "iife",
-        name: "VueCompositions",
-        exports: "named",
+        file: outputDir + 'index.min.js',
+        format: 'iife',
+        name: 'VueCompositions',
+        exports: 'named',
         extend: true,
         globals: {
-          vue: "Vue",
-        },
-      },
+          vue: 'Vue',
+          'vue-demi': 'VueDemi'
+        }
+      }
     ],
     plugins: [
       // 打包插件
       resolve(), // 查找和打包node_modules中的第三方模块
       commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
       typescript(), // 解析TypeScript
-      terser(),
+      terser()
     ],
-    external: ["vue", "vue-demi"],
+    external: ['vue', 'vue-demi']
   },
   {
     // 生成 .d.ts 类型声明文件
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
       file: outputDir + pkg.types,
-      format: "es",
+      format: 'es'
     },
-    plugins: [dts()],
-  },
-];
+    plugins: [dts()]
+  }
+]
